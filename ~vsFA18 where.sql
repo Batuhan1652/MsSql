75c9c946439a495628164762d097a192bc1b4054
Batuhan Metin <102637118+Batuhan1652@users.noreply.github.com>
2023-01-27 10:30:02 +0300

    Update ~vsFA18 where.sql

--- ~vsFA18 where.sql
+++ ~vsFA18 where.sql
@@ -1,11 +1,11 @@
 select * from Customers_Tables
-Where City = 'Bursa'  /* Bu kod Bursa'da yaşayan herkezi çağırdı */
+Where City = 'Bursa'  /* Bu kod Bursa'da yaÅŸayan herkezi Ã§aÄŸÄ±rdÄ± */
 
 
 select * from Customers_Tables
-Where City = 'Bursa' AND District = 'Orhangazi'  /* Bu kod sadece Bursa, Orhangazi'de yaşayanları çağırdı */
+Where City = 'Bursa' AND District = 'Orhangazi'  /* Bu kod sadece Bursa, Orhangazi'de yaÅŸayanlarÄ± Ã§aÄŸÄ±rdÄ± */
 
-select * from Customers_Tables  /* Bu kod ise Bursa'da yaşayan fakat Orhangazi'de yaşamayan insanları çağırdı*/
+select * from Customers_Tables  /* Bu kod ise Bursa'da yaÅŸayan fakat Orhangazi'de yaÅŸamayan insanlarÄ± Ã§aÄŸÄ±rdÄ±*/
 Where  City =  'Bursa' AND District <> 'Orhangazi'
 
 select * from Customers_Tables
@@ -19,35 +19,35 @@ where Age
 between 20 and 25
 
 select * from Customers_Tables
-where Customer_Name Like 'Rümeysa%' /* rümeysa ile başlıyanları getir demiş olduk o yüzde ona yarıyor */
+where Customer_Name Like 'RÃ¼meysa%' /* rÃ¼meysa ile baÅŸlÄ±yanlarÄ± getir demiÅŸ olduk o yÃ¼zde ona yarÄ±yor */
 
 select * from Customers_Tables
-where Customer_Name Like '%İNCE%' /* İsmi veya soyisminde ince olanları getiriyor fakat illa ince olmak zorunead değil */
+where Customer_Name Like '%Ä°NCE%' /* Ä°smi veya soyisminde ince olanlarÄ± getiriyor fakat illaki ince olmak zorunead deÄŸil */
 
 select * from Customers_Tables
-where Customer_Name Like '%Örnek' /* sonu örnek ile biter diyenleri getir demiş olduk o yüzde ona yarıyor */
-
-select * from Customers_Tables
-where City= 'Isparta'
-and District in ('Uluborlu', 'Yalvaç') /* ıspartada ve uluborlu,yalvaçta yaşayan müşterileri getirdi */
+where Customer_Name Like '%Ã–rnek' /* sonu Ã¶rnek ile biter diyenleri getir demiÅŸ olduk o yÃ¼zde ona yarÄ±yor */
 
 select * from Customers_Tables
 where City= 'Isparta'
-and District not in ('Uluborlu', 'Yalvaç') /* ıspartada ve uluborlu,yalvaçta dışında yaşayan müşterileri getirdi */
+and District in ('Uluborlu', 'YalvaÃ§') /* Isparta'da, Uluborlu ve YalvaÃ§'ta yaÅŸayan mÃ¼ÅŸterileri getirdi */
 
 select * from Customers_Tables
 where City= 'Isparta'
-and District not in ('Uluborlu', 'Yalvaç')
+and District not in ('Uluborlu', 'YalvaÃ§') /* Isparta'da, Uluborlu ve YalvaÃ§ dÄ±ÅŸÄ±nda yaÅŸayan mÃ¼ÅŸterileri getirdi */
+
+select * from Customers_Tables
+where City= 'Isparta'
+and District not in ('Uluborlu', 'YalvaÃ§')
 and Gender = 'K'
 
 Update Customers_Tables Set Gender = 'Erkek'
-Where Gender = 'E'  /* Tüm E yazan şeyleri Erkek olarak değiştirdik */
+Where Gender = 'E'  /* TÃ¼m E yazan ÅŸeyleri Erkek olarak deÄŸiÅŸtirdik */
 
 select * from Customers_Tables
 
-delete from Customers_Tables where ID = 1 /* ID'si 1 olanı siler */
+delete from Customers_Tables where ID = 1 /* ID'si 1 olanÄ± siler */
 
-delete from Customers_Tables where Customer_Name like 'Serhat%' /* tüm serhat ile başlayanları siler */
+delete from Customers_Tables where Customer_Name like 'Serhat%' /* tÃ¼m Serhat ile baÅŸlayanlarÄ± siler */
 
 
 

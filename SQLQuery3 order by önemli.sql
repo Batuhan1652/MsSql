68dbf35402b3e58b266868aec99ed118ef9bb7b0
Batuhan Metin <102637118+Batuhan1652@users.noreply.github.com>
2023-01-27 10:26:13 +0300

    Update SQLQuery3 order by önemli.sql

--- SQLQuery3 order by önemli.sql
+++ SQLQuery3 order by önemli.sql
@@ -1,41 +1,41 @@
--- ÜRÜN KATEGORÝLERÝNE GÖRE SATIÞ DATASI --
+-- ÃœRÃœN KATEGORÄ°LERÄ°NE GÃ–RE SATIÃž DATASI --
 
 select CATEGORY_NAME1, sum(LINENET) as Satis_Toplami from SALES
 where CATEGORY_NAME1 is not NULL
 group by CATEGORY_NAME1 
 order by Satis_Toplami desc
 
--- MARKALARINI DA EKLÝYELÝM --
+-- ÃœRÃœN KATEGORÄ°SÄ° VE MARKALARA GÃ–RE --
 
 select CATEGORY_NAME1, sum(LINENET) as Satis_Toplami, BRAND from SALES
 where CATEGORY_NAME1 is not NULL
 group by CATEGORY_NAME1, BRAND 
 order by Satis_Toplami  desc
 
--- GIDA CATEGORÝSÝN DE HANGÝ MARKANIN DAHA ÇOK SATILDIÐINI ÝNCELÝYORUZ--
+-- GIDA KATEGORÄ°SÄ°NDE HANGÄ° MARKALARIN EN Ã‡OK SATILDIÄžINI Ä°NCELEME--
 
 select CATEGORY_NAME1, sum(LINENET) as Satis_Toplami, BRAND from SALES
-where CATEGORY_NAME1 = 'Gýda' 
+where CATEGORY_NAME1 = 'GÃ½da' 
 group by CATEGORY_NAME1, BRAND 
 order by Satis_Toplami  desc
 
--- ÜRÜN MARKASINA GÖRE SATIÞLAR--
+-- ÃœRÃœN MARKASINA GÃ–RE SATIÅžLAR--
 
 select BRAND, SUM(LINENET) as Satis_Toplami from SALES
-WHERE BRAND is not null -- Bursa is not null eklemediðimz için hata oldu anlayamadým sebebini araþtýr --
+WHERE BRAND is not null 
 group by BRAND 
 order by Satis_Toplami  desc
 
--- Ülker'in hangi kategorilerde daha çok satýþ yaptýðýný inceledik --
+-- Ãœlker MarkasÄ±nÄ±n Hangi Kategorilerde En Ã‡ok SatÄ±ÅŸ YaptÄ±ÄŸÄ±nÄ± Ä°nceleme --
 
 select BRAND, SUM(LINENET) as Satis_Toplami, CATEGORY_NAME1 from SALES
-WHERE BRAND = 'Ülker' 
+WHERE BRAND = 'Ãœlker' 
 group by CATEGORY_NAME1, BRAND 
 order by Satis_Toplami  desc
 
--- 2 Kategoriye kadar inceleme 2 kýrýlým --
+-- 2 Kategoriye kadar inceleme 2 kÄ±rÄ±lÄ±mlÄ± --
 
 select BRAND, SUM(LINENET) as Satis_Toplami, CATEGORY_NAME1, CATEGORY_NAME2 from SALES
-WHERE BRAND = 'Ülker' 
+WHERE BRAND = 'Ãœlker' 
 group by CATEGORY_NAME1, BRAND, CATEGORY_NAME2 
 order by CATEGORY_NAME1, BRAND, CATEGORY_NAME2, Satis_Toplami desc

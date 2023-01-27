f91647b4197c4317260adab37f59bf16a7f6e2f1
Batuhan Metin <102637118+Batuhan1652@users.noreply.github.com>
2023-01-27 10:22:34 +0300

    Update SQLQuery2(top kumutu).sql

--- SQLQuery2(top kumutu).sql
+++ SQLQuery2(top kumutu).sql
@@ -1,16 +1,16 @@
 select * from Customers_Tables
 order by Birth_Date  
-/* order by sıralamaya yarar yukarıdaki komut bize en şalı olandan en gence doğru getirdi asc dersek a'dan z'ye desc dersek z'den a'ya şeklinde sıralar*/
+/* order by sÄ±ralamaya yarar yukarÄ±daki komut bize en yaÅŸlÄ± olandan en gence doÄŸru getirdi asc dersek a'dan z'ye desc dersek z'den a'ya ÅŸeklinde sÄ±ralar*/
 
 select * from Customers_Tables
 order by City asc, Birth_Date desc
 
-select TOP 5* from Customers_Tables  /* Bize ilk 5 değeri çevirdi*/
+select TOP 5* from Customers_Tables  /* Bize ilk 5 deÄŸeri Ã§evirdi*/
 
 select TOP 10* from Customers_Tables
-where City = 'İstanbul'
+where City = 'Ä°stanbul'
 order by Age
 
-select TOP 10 percent*  from Customers_Tables /* burda da yüzde 10'nu aldı */
-where City = 'İstanbul'
+select TOP 10 percent*  from Customers_Tables /* burda da yÃ¼zde 10'nu aldÄ± */
+where City = 'Ä°stanbul'
 order by Age
